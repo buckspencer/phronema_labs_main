90aa9cf55db1e0d115be9e0b5e4e258c5588aaca
Buck Edward Spencer <ad3uck@gmail.com>
2025-04-20 17:23:58 -0700

    Update Logo component to adjust width for improved responsiveness

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,5 +5,5 @@ import logo from '../images/logo-1.svg'
 import logoDark from '../images/logo-1-dark.svg'
 
 export function Logo({ isDark = false }) {
-  return <Image src={isDark ? logoDark : logo} alt="Logo" width={200} className="sm:w-[392px] w-[200px]" />
+  return <Image src={isDark ? logoDark : logo} alt="Logo" width={150} className="sm:w-[392px] w-[150px]" />
 }
